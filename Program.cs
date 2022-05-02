bfa1b8f34575b3db9d348d29d90ba061d930ee83
EvgenyKoshchin <104790862+EvgenyKoshchin@users.noreply.github.com>
2022-05-02 21:56:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CholeskyLab1CCharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] threadsNum = { 2, 4, 6, 8 };
+
+            int[] matrixDimension = { 10, 100, 1000, 3000, 6000 };
+
+            foreach (int i in matrixDimension)
+            {
+
+                Console.WriteLine("Размерность матрицы: " + i);
+
+                Double[,] matrixA = MatrixBuilder.createSymmetricMatrix(i);
+                Double[] vectorB = MatrixBuilder.getRow(matrixA, 0);
+
+                Stopwatch Watch = new Stopwatch();
+
+                Watch.Start();
+
+                Double[] res1 = SolveByCholesky.solveLinearSystem(matrixA, vectorB);
+
+                Watch.Stop();
+
+                Console.WriteLine("Время выполнения последовательного алгоритма: "
+                        + Watch.Elapsed);
+
+                foreach (int j in threadsNum)
+                {
+                    Watch = new Stopwatch();
+                    Watch.Start();
+                    Double[] res2 = SolveByCholeskyParallel.solveLinearSystemParallel(matrixA, vectorB, j);
+                    Watch.Stop();
+                    Console.WriteLine("Время выполнения параллельного алгоритма при " + j + " потоках(е)" + ": "
+                            + Watch.Elapsed);
+                }
+                Console.WriteLine("\n");
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
